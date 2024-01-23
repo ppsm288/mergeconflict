6f3a3c82018f6e86085a426f8e43aa315f8046c7
ppsm288 <ppsm288@gmail.com>
2024-01-23 11:16:12 -0600

    added code

---
+++ code.java
@@ -0,0 +1,12 @@
+public class GreetingApp {
+    public static void main(String[] args) {
+        greetWorld();
+    }
+
+    private static void greetWorld() {
+        String greeting = "Hello, World!";
+        System.out.println(greeting);
+    }
+}
+
+  
